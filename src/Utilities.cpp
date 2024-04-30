e0e0388ca52f8e3a0e8934eeaa58162b1c7b566d
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-04-30 16:12:03 +0200

    update

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -1,20 +1,22 @@
 /**
  * @file Utilities.cpp
  * @brief [Description of file]
- * 
+ *
  * @author Filip Dabkowski
  * @date 25/04/2024
  */
 #include "Utilities.h"
 
+#include <cstdio>
+
 
 int inputNumber(char divider) {
- int number = 0;
+	int number = 0;
 
- char ch;
- while ((ch = (char) getchar()) != divider) {
-  number = number * 10 + (ch - '0');
- }
+	char ch;
+	while ((ch = (char) getchar()) != divider) {
+		number = number * 10 + (ch - '0');
+	}
 
- return number;
+	return number;
 }
